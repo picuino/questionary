50fe1565ce49bd44f6e9904a393d36403e01cfae
Carlos Pardo <carlos@picuino.com>
2021-02-22 10:30:37 +0100

    Updated images

---
+++ images/new_images.py
@@ -0,0 +1,11 @@
+import os
+import codecs
+import re
+
+licenses = codecs.open('../Licenses.md', 'r', encoding='utf-8').read()
+
+images = [f for f in os.listdir('.') if f[-4:].lower() in ['.png', '.gif', '.jpg']]
+
+for image in images:
+    if not re.search('images/thumbs/' + image, licenses):
+        print('![](images/thumbs/%s)' % image)
