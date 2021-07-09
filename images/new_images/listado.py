0012a14f98a05e5a782cddbfee1f0ceb93bb1c86
Carlos Pardo <carlos@picuino.com>
2021-07-09 12:43:20 +0200

    Actualizado a 09/07/2021

--- images/new_images/listado.py
+++ images/new_images/listado.py
@@ -1,5 +1,5 @@
 import os
 for filename in os.listdir('.'):
-    if filename[-4:].lower() in ['.png']:
+    if filename[-4:].lower() in ['.png', '.jpg']:
         print('![](thumbs/%s)' % filename)
 input('Pulsa Enter')
