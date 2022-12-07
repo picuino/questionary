d0603bab9e5bd8c09284fa788898ef4263c5d2d6
Carlos Pardo <carlos@picuino.com>
2022-12-07 20:36:02 +0100

    Cambios a 07/12/2022

--- source-cloze/es-mecan-calibre-medidas.py
+++ source-cloze/es-mecan-calibre-medidas.py
@@ -7,6 +7,7 @@ Title: Medidas con calibre
 Copyright: 2022 por Carlos Pardo Martín
 License: Creative Commons Attribution-ShareAlike 4.0
 License_link: https://creativecommons.org/licenses/by-sa/4.0/
+Source_link: https://www.picuino.com/es/mecan-calibre.html
 Show_max: 30
 Num_tries: 1
 
@@ -21,14 +22,14 @@ question_template = """
   Cloze:
     ¿Qué medida está realizando este calibre?
     <br><br>
-    Medida = {%s} mm
+    Medida = {%s|%s} mm
 """
 
 data = [header]
 for i in range(301):
-    medida = '%0.1f' % (i/10.0)
-    medida = re.sub('\.', ',', medida)
-    question = question_template % (medida, i, medida)
+    medida_punto = '%0.1f' % (i/10.0)
+    medida_coma = re.sub('\.', ',', medida_punto)
+    question = question_template % (medida_coma, i, medida_coma, medida_punto)
     data.append(question)
 with codecs.open('es-mecan-calibre-medidas.yaml', 'w', encoding='utf-8-sig') as fo:
     fo.write(''.join(data))
